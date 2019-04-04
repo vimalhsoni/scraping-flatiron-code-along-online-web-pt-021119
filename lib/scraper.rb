f5f7469b11254b10fca181afbf29f44059c6f782
Vimal Soni <vimal.h.soni@gmail.com>
2019-04-04 03:17:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,7 @@ class Scraper
   def make_courses
     self.get_courses.each do |post|
       course = Course.new
-      binding.pry
+      # binding.pry
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
